dde12d870e27dea0cf88b41df46a2a0cf93b8e80
JPercival22 <you@example.com>
2023-07-02 22:12:33 +0100

    minimised all images to webp format - updated JS to pass Lint - added content to guitar case study page

--- dynamic-script.js
+++ dynamic-script.js
@@ -1,3 +1,4 @@
+let navbar = document.querySelector("#navbar");
 const navbarToggle = navbar.querySelector('#navbar-toggle');
 let isNavbarExpanded = navbarToggle.getAttribute('aria-expanded') === 'true';
 
