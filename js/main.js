d9ef3adfd7279d66fd3c5b1cf30908e49b72b44a
JPercival22 <you@example.com>
2023-12-30 03:57:56 +0000

    refactored image module mjs and main js to change images on response, added interactive headline, made index page more responsive, interactive and reduced css

--- js/main.js
+++ js/main.js
@@ -1,39 +1,64 @@
-import { addToggleListener, removeToggleListener } from './menuModule.mjs';
+import { addToggleListener, removeToggleListener } from './menuModule.mjs'
+import { setResponsiveImages } from './responsiveImageModule.mjs';
 
 // Function to toggle the menu
-function toggleMenu() {
-  document.body.classList.toggle("mobile-menu-open");
+function toggleMenu () {
+  document.body.classList.toggle('mobile-menu-open')
 }
 
 // Add event listener to toggle the menu on user interaction
-addToggleListener();
+addToggleListener()
 
-
-// Example condition 1 for importing and using module1
+// Example condition 1 for importing and using responsive image module
 if (window.innerWidth < 768) {
-  import('./responsiveImageModule.mjs').then((module1) => {
-    // Use module1 functionality here
-    module1.setResponsiveImage(); // Assuming module1 exports a function named setResponsiveImage
-  }).catch((error) => {
-    console.error("An error occurred while importing module1:", error);
-  });
+  import('./responsiveImageModule.mjs')
+    .then(({ setResponsiveImages }) => {
+      const imageData = [
+        {
+          elementId: 'ux-guitar',
+          smallImagePath: '/assets/images/guitar-lessons-banner.webp',
+          largeImagePath: '/assets/images/guitar-lessons-banner.webp'
+        },
+        {
+          elementId: 'syn-banner', 
+          smallImagePath: '/assets/images/syn-banner.webp',
+          largeImagePath: '/assets/images/syn-banner.webp'
+        },
+        { 
+          elementId: 'come-together-banner', 
+          smallImagePath: '/assets/images/Come-Together-Banner.webp',
+          largeImagePath: '/assets/images/Come-Together-Banner.webp'
+        }
+        // Add more image data objects here
+      ];
+
+      setResponsiveImages(imageData);
+    })
+    .catch(error => {
+      console.error('An error occurred while importing module:', error);
+    });
 }
 
-// Example condition 2 for importing and using module2
+
+// Example condition 2 for importing and using slider module
 if (document.getElementById('sidebar') !== null) {
-  import('./sideBarModule.mjs').then((module2) => {
-    // Use module2 functionality here
-    module2.toggleSidebar(); // Assuming module2 exports a defaultExportedFunction
-  }).catch((error) => {
-    console.error("An error occurred while importing module2:", error);
-  });
+  import('./sideBarModule.mjs')
+    .then(module2 => {
+      // Use module2 functionality here
+      module2.toggleSidebar() // Assuming module2 exports a defaultExportedFunction
+    })
+    .catch(error => {
+      console.error('An error occurred while importing module2:', error)
+    })
 }
 // Add more condition checks and dynamic imports as needed for other modules
 if (document.querySelector('.read-more') !== null) {
-  import('./readMore.mjs').then((readMoreModule) => {
-    // Use readMoreModule functionality here
-    readMoreModule.initReadMore();
-  }).catch((error) => {
-    console.error("An error occurred while importing readMoreModule:", error);
-  });
+  import('./readMore.mjs')
+    .then(readMoreModule => {
+      // Use readMoreModule functionality here
+      readMoreModule.initReadMore()
+    })
+    .catch(error => {
+      console.error('An error occurred while importing readMoreModule:', error)
+    })
 }
--- js/responsiveImageModule.mjs
+++ js/responsiveImageModule.mjs
@@ -1,15 +1,17 @@
-function setResponsiveImage(elementId, smallImagePath, largeImagePath) {
-  const imageElement = document.getElementById(elementId);
-  if (!imageElement) {
-    console.error('Element with id ' + elementId + ' not found');
-    return; // Exit the function early if the element is not found
-  }
+function setResponsiveImages(images) {
+  images.forEach((image) => {
+    const imageElement = document.getElementById(image.elementId);
+    if (!imageElement) {
+      console.error('Element with id ' + image.elementId + ' not found');
+      return; // Exit the function early if the element is not found
+    }
 
-  if (window.innerWidth < 768) {
-    imageElement.src = smallImagePath;
-  } else {
-    imageElement.src = largeImagePath;
-  }
+    if (window.innerWidth < 768) {
+      imageElement.src = image.smallImagePath;
+    } else {
+      imageElement.src = image.largeImagePath;
+    }
+  });
 }
 
-export { setResponsiveImage };
+export { setResponsiveImages };
