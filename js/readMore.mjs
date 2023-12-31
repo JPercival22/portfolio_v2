b0ea69050dd8609a6fecab0ed7434557983fd1e4
JPercival22 <you@example.com>
2023-12-31 10:22:29 +0000

    index page code base refactored and reduced

--- js/readMore.mjs
+++ js/readMore.mjs
@@ -1,16 +1,33 @@
-// readMore.js
-
 export function initReadMore() {
-    document.addEventListener('click', function(event) {
-      if (event.target.matches('.read-more')) {
-        event.preventDefault();
-        const readMoreLink = event.target;
-        const moreContent = readMoreLink.previousElementSibling;
-  
+  const readMoreLinks = document.querySelectorAll('.read-more');
+  const moreContents = document.querySelectorAll('.more-content');
+
+  function openReadMoreDefault() {
+    if (window.innerWidth >= 992) {
+      moreContents.forEach(moreContent => {
         moreContent.style.display = 'inline';
-        readMoreLink.style.display = 'none';
         moreContent.classList.add('show-more');
-      }
-    });
+      });
+
+      readMoreLinks.forEach(readMoreLink => {
+        readMoreLink.style.display = 'none';
+      });
+    }
   }
-  
+
+  document.addEventListener('click', function(event) {
+    if (event.target.matches('.read-more')) {
+      event.preventDefault();
+      const readMoreLink = event.target;
+      const moreContent = readMoreLink.previousElementSibling;
+
+      moreContent.style.display = 'inline';
+      readMoreLink.style.display = 'none';
+      moreContent.classList.add('show-more');
+    }
+  });
+
+  openReadMoreDefault(); // Call the function to set default state
+
+  window.addEventListener('resize', openReadMoreDefault); // Adjust when the window is resized
+}
