6d03b62bae4387cb0b6be90ea573692fa33f8433
JPercival22 <you@example.com>
2024-04-02 17:44:31 +0100

    fixed flex issue in mobile nav bar, refactored secction dimensions - TODO, make sections responsive. refactor ambiguous wrapper -not conventional

--- js/navigationModule.mjs
+++ js/navigationModule.mjs
@@ -1,12 +1,19 @@
 // navigation.mjs
 const navbar = document.getElementById('navbar');
+const logoContainer = document.querySelector('.logo-container');
 
 function handleScroll() {
   window.addEventListener('scroll', function() {
     if (window.scrollY > 0) {
       navbar.classList.add('scrolled');
+      if (logoContainer) {
+        logoContainer.style.opacity = 1;
+      }
     } else {
       navbar.classList.remove('scrolled');
+      if (logoContainer) {
+        logoContainer.style.opacity = 0;
+      }
     }
   });
 }
